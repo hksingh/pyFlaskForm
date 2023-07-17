7b03ba0253e6dfc868b8bfa9650dfb6ab5d50f9d
Hemant Singh <singh.hemant@gmail.com>
2023-07-16 22:54:24 -0400

    views and scss

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
@@ -11,4 +11,4 @@ def logout():
 
 @auth.route('/signup')
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign-up.html")
